2da21597c75a300862874ff2d6dd0edb02eaca2b
swagath shetty <swagath.shetty@gmail.com>
2022-02-08 17:10:21 +0530

    created my first reusable flutter widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main()=> runApp(MaterialApp(
-    home:Scaffold(
+    home:Home()
+  ));
+
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title:Text('My first app'),
-        centerTitle: true,
-        backgroundColor:Colors.red[600]
+          title:Text('My first app'),
+          centerTitle: true,
+          backgroundColor:Colors.red[600]
       ),
       body:Center(
         child: Text(
             'Hello ninjas',
-          style:TextStyle(
-            fontSize:20,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2,
-            color:Colors.grey[600],
-            fontFamily:'IndieFlower'
-          )
+            style:TextStyle(
+                fontSize:20,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+                color:Colors.grey[600],
+                fontFamily:'IndieFlower'
+            )
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -24,8 +33,6 @@ void main()=> runApp(MaterialApp(
           child:Text('click'),
           backgroundColor:Colors.red[600]
       ),
-    ),
-  ));
-
-
-//
+    );
+  }
+}
